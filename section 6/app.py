e6713eefbd383e163cff1a4b1835a0eae3a47c07
Volpao <tatiana.a.kraeva@gmail.com>
2021-10-14 13:57:11 +0100

    db changes

--- section 6/app.py
+++ section 6/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-from db import db
+
 from resources.store import Store, StoreList
 from security import authenticate, identity
 from resources.user import UserRegister
@@ -24,6 +24,7 @@ api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 if __name__=='__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
 
