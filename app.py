2c370ad6b4c81fdc1279be928b593de98d87c771
Volpao <tatiana.a.kraeva@gmail.com>
2021-10-14 18:11:11 +0100

    heroku fixes

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-from db import db
 from resources.store import Store, StoreList
 from security import authenticate, identity
 from resources.user import UserRegister
@@ -15,10 +14,6 @@ app.secret_key = 'fokifoki'
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Store, '/store/<string:name>')
@@ -26,7 +21,12 @@ api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 if __name__=='__main__':
-    
+    from db import db
     db.init_app(app)
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+            
     app.run(port=5000, debug=True)
 
